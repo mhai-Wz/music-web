69d4a5b6ad5a3478cd8d6f84d53a98299d7fa9d1
mhai-Wz <haduyminhhai8714@gmail.com>
2026-05-01 03:40:00 +0700

    fix render port

--- backend/server.js
+++ backend/server.js
@@ -113,6 +113,8 @@ app.post("/upload", upload.single("file"), async (req, res) => {
 /* ======================
    START SERVER
 ====================== */
-app.listen(5000, () => {
-  console.log("Server running http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
